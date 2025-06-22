1c33c55e311e5c43e1384f3078026dd88df61e35
BasithMrasak <basithmrasak1@gmail.com>
2025-06-22 22:47:02 +0530

    use env variable for api key

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect, url_for
 import requests
-from config import API_KEY
+import os
+API_KEY = os.environ.get("API_KEY")
 
 app=Flask(__name__)
 
